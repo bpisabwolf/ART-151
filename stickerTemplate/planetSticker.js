97843ac36d88eddc0d2998e42ab374b2a61a1c55
Boris P <32775983+bpisabwolf@users.noreply.github.com>
2020-02-12 09:38:39 -0600

    Adding workshop code

---
+++ stickerTemplate/planetSticker.js
@@ -0,0 +1,33 @@
+//Author: Boris Pisabaj
+//Purpose: for ART 151 Spring 2020
+//Written using p5 library
+//~(0-0)~
+
+
+function preload(){
+
+}
+
+
+function setup(){
+
+}
+function draw(){
+
+}
+
+
+function keyPressed(){
+
+}
+
+
+function mousePressed(){
+
+
+}
+
+
+function windowResized(){
+  createCanvas(windowWidth, windowHeight);
+}
--- stickerTemplate/sketch.js
+++ stickerTemplate/sketch.js
@@ -1,7 +1,21 @@
+let nums = [4,9,16,25,36,49, 64,81, 100]
+let modifier = 1;
+
+let inc = true;
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  print(nums[2]);
+  print(nums.length);
+  ellipse(windowWidth/2, windowHeight/2, nums[2], nums[2]);
 }
 
 function draw() {
+  var i;
+  for(i = 0; i < nums.length; i++){
+    stroke(10);
+    ellipse(random(windowWidth), random(windowHeight), random(nums[i]) * modifier, random(nums[i]) * modifier);
+  }
+  modifier = modifier * 1.01;
   // put drawing code here
 }

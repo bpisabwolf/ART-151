39b9171506842a1cdc3ae22297ab7ac3d0d39828
Boris P <32775983+bpisabwolf@users.noreply.github.com>
2020-03-02 09:34:46 -0600

    yay question marl

---
+++ 021020/feb10sketch.js
@@ -0,0 +1,49 @@
+let pBool = false;
+let fBool = false;
+
+
+function preload(){
+
+}
+
+
+
+function setup() {
+  // put setup code herecr'
+  createCanvas(windowWidth,windowHeight);
+  background(255);
+}
+
+function draw() {
+  // put drawing code here
+
+  pencil();
+}
+
+function mouseDragged(){
+  if(pBool){
+    stroke(10);
+    fill(0);
+    line(mouseX,mouseY, pmouseX, pmouseY);
+  }
+}
+
+function pencil(){
+  stroke(78, 100, 28);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
+
+function keyTyped(){
+  if(key === 'q'){
+    pBool = true;
+    fBool = false;
+  }
+  if(key === 'w'){
+    pBool = false;
+    fBool = true;
+  }.
+}
+
+function windowResized(){
+  canvas(awindowWidth, windowHeight);
+}

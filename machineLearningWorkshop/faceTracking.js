94c9a552217f764e8035913fd12e1d2c51ba6feb
Boris P <32775983+bpisabwolf@users.noreply.github.com>
2020-04-15 07:47:19 -0500

    yes

--- machineLearningWorkshop/faceTracking.js
+++ machineLearningWorkshop/faceTracking.js
@@ -47,8 +47,11 @@ function draw() {
     noStroke();
     for (let i = 0; i < positions.length; i++) {
         fill(map(i, 0, positions.length, 0, 360), 50, 100);
-        ellipse(positions[i][0], positions[i][1], 4, 4);
-        text(i, positions[i][0], positions[i][1]);
+        // ellipse(positions[i][0], positions[i][1], 4, 4);
+        // text(i, positions[i][0], positions[i][1]);
+
+        ellipse(positions[44][0], positions[44][1], 30, 30);
+        ellipse(positions[50][0], positions[50][1], 30, 30);
     }
 
     if (positions.length > 0) {
